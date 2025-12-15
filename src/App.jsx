4dd34534400a760d79c241e1f6a186fb0eb91b11
strictlyManish <rajzmanish94@gmail.com>
2025-12-15 23:40:31 +0530

    done with home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import Navigation from './components/Navigation'
-import Mainroutes from './routes/Mainroutes'
-
+import Navigation from './components/Navigation';
+import Mainroutes from './routes/Mainroutes';
 function App() {
   return (
-    <div className='h-screen w-screen bg-gray-800 text-white py-4 overflow-hidden'>
-      <Navigation/>
-      <Mainroutes/>
+    <div className="h-screen w-screen bg-linear-to-br from-gray-900 via-gray-800 to-black text-white py-4 overflow-hidden">
+      <Navigation />
+      <Mainroutes />
     </div>
   )
 }
