ec736ee1af8c2766d6e3eccb5191375743ce0b27
strictlyManish <rajzmanish94@gmail.com>
2025-12-23 21:33:50 +0530

    adding loader

--- src/routes/Mainroutes.jsx
+++ src/routes/Mainroutes.jsx
@@ -7,10 +7,13 @@ const Project = lazy(() => import('../pages/Project'))
 const Contact = lazy(() => import('../pages/Contact'))
 const About = lazy(() => import('../pages/About'))
 const ImageEditor = lazy(() => import('../components/ImageEditor'))
+import Loader from './../components/Loader';
 
 function Mainroutes() {
   return (
-    <Suspense fallback={<div className="text-center mt-10">Loading... </div>}>
+    <Suspense fallback={<div className="h-screen flex justify-center items-center">
+          <Loader />
+        </div>}>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/project' element={<Project />} />
